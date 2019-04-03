e465d1c142c1d784a908626dfb421eb844e9226e
HobbyRobotic <daniel.ruzewicz@pwc.com>
2019-04-03 15:20:45 +0200

    Rework na wzór pythona - nie działa anyway

--- TestCamera/HCSR04.cs
+++ TestCamera/HCSR04.cs
@@ -17,79 +17,54 @@ namespace TestCamera
             GpioController controller = GpioController.Instance;
 
             //initialize trigger pin.
-            this.triggerPin = controller.Pin27;
-            this.triggerPin.PinMode = GpioPinDriveMode.Output;
+			triggerPin = controller.Pin18;
+            triggerPin.PinMode = GpioPinDriveMode.Output;
 
             //initialize echo pin.
-            this.echoPin = controller.Pin28;
+            this.echoPin = controller.Pin24;
             this.echoPin.PinMode = GpioPinDriveMode.Input;
-
-
         }
 
-        private double LengthOfHighPulse
-        {
-            get
-            {
-                // The sensor is triggered by a logic 1 pulse of 10 or more microseconds.
-                // We give a short logic 0 pulse first to ensure a clean logic 1.
-                this.triggerPin.Write(GpioPinValue.Low);
-                Sleep(5);
-                this.triggerPin.Write(GpioPinValue.High);
-                Sleep(10);
-                this.triggerPin.Write(GpioPinValue.Low);
-
-                // Read the signal from the sensor: a HIGH pulse whose
-                // duration is the time (in microseconds) from the sending
-                // of the ping to the reception of its echo off of an object.
-                return GetTimeUntilNextEdge(echoPin, GpioPinValue.High, 100);
-
-
-            }
-        }
-
+		private static Stopwatch stopWatch = new Stopwatch();
+              
         public double Distance
         {
             get
-            {
-                // convert the time into a distance
-                // duration of pulse * speed of sound (343m/s)
-                // remember to divide by two because we're measuring the time for the signal to reach the object, and return.
-                return (SPEED_OF_SOUND_METERS_PER_SECOND / 2) * LengthOfHighPulse;
+            {              
+				triggerPin.Write(GpioPinValue.High);
+                Sleep(100000);
+                this.triggerPin.Write(GpioPinValue.Low);
+
+				var t = Task.Run(() =>
+			   {
+				   stopWatch.Reset();
+
+				   while (!echoPin.Read())
+				   {
+					   stopWatch.Start();
+				   }
+
+				   while (echoPin.Read())
+				   {
+					   stopWatch.Stop();
+				   }
+
+				   return stopWatch.Elapsed.TotalSeconds;
+				});
+                
+				bool isCompleted = t.Wait(TimeSpan.FromMilliseconds(200));
+
+                if (isCompleted)
+                {
+					return (SPEED_OF_SOUND_METERS_PER_SECOND / 2) * t.Result;
+                   
+                }
+                else
+                {
+					return -1d;
+                }                     
             }
-        }
-
-
-        private static Stopwatch stopWatch = new Stopwatch();
-
-        public static double GetTimeUntilNextEdge(GpioPin pin, GpioPinValue edgeToWaitFor, int maximumTimeToWaitInMilliseconds)
-        {
-            var t = Task.Run(() =>
-            {
-                stopWatch.Reset();
-
-                while (pin.ReadValue() != edgeToWaitFor) { }
-
-                stopWatch.Start();
-
-                while (pin.ReadValue() == edgeToWaitFor) { }
-
-                stopWatch.Stop();
-
-                return stopWatch.Elapsed.TotalSeconds;
-            });
-
-            bool isCompleted = t.Wait(TimeSpan.FromMilliseconds(maximumTimeToWaitInMilliseconds));
-
-            if (isCompleted)
-            {
-                return t.Result;
-            }
-            else
-            {
-                return -1d;
-            }
-        }
+        }               
 
         private static ManualResetEvent manualResetEvent = new ManualResetEvent(false);
 
--- TestCamera/Program.cs
+++ TestCamera/Program.cs
@@ -10,33 +10,40 @@ namespace TestCamera
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World2!2323dgffgd23");
-            //TestCaptureImage();
-            TestLedBlinking();
+             TestCaptureImage();
+			// TestLedBlinking();
+			TestMeasurement();
         }
+
         static void TestCaptureImage()
         {
+			Console.WriteLine("Hello World2!2323dgffgd23");
+
             var pictureBytes = Pi.Camera.CaptureImageJpeg(640, 480);
             var targetPath = "/home/pi/picture.jpg";
             if (File.Exists(targetPath))
                 File.Delete(targetPath);
 
             File.WriteAllBytes(targetPath, pictureBytes);
-            Console.WriteLine($"Took picture -- Byte count: {pictureBytes.Length}");
+            Console.WriteLine($"Took picture -- Byte count: {pictureBytes.Length}");                      
+        }
 
-            var sensor = new HCSR04();
+		static void TestMeasurement()
+		{
+			var sensor = new HCSR04();
             for (var i = 0; i < 20; i++)
             {
                 Console.WriteLine($"Obecny dystans : {sensor.Distance}");
                 System.Threading.Thread.Sleep(2000);
             }
-        }
+		}
 
 
         public static void TestLedBlinking()
         {
             // Get a reference to the pin you need to use.
             // All 3 methods below are exactly equivalent
-            var blinkingPin = Pi.Gpio[18];
+            var blinkingPin = Pi.Gpio[21];
             
 
             // Configure the pin as an output
